8cd19052494c32b38bd06ba4e9c4436158cda080
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-09-28 14:27:30 +0600

    featured cars done

--- src/Pages/Home/Hero/FeaturedCars/FeateuredCars.tsx
+++ src/Pages/Home/Hero/FeaturedCars/FeateuredCars.tsx
@@ -1,23 +1,34 @@
 import { useGetAllCarsQuery } from "@/redux/features/Car/carApi";
-import { TCar } from "@/redux/features/Car/carSlice";
+import GridColumn from "./GridColumns";
+
+
 
 const FeateuredCars = () => {
   const { data: allCars } = useGetAllCarsQuery(undefined);
 
-  console.log(allCars);
+
+  const columns = [
+    allCars?.data?.slice(0, 6),
+    allCars?.data?.slice(6, 12),
+    allCars?.data?.slice(12, 16),
+  ];
+
   return (
-    <div className="scroll-container">
-    <div className="scroll-content">
-      <div className="masonry sm:masonry-sm md:masonry-md">
-        {allCars?.data?.map((car:TCar) => (
-          <div key={car?._id} className="break-inside rounded-lg overflow-hidden">
-            <img src={car?.image} alt={car?.name} className="w-full h-full object-cover" />
-          </div>
+    <div className="flex flex-col py-4">
+      <h1 className="text-color text-xl font-bold text-center my-4 underline">
+        Explore the best <span className="text-orange-500">Driving Machine</span>
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {columns.map((cars, index) => (
+          <GridColumn key={index} cars={cars} />
         ))}
       </div>
     </div>
-  </div>
   );
 };
+
+
+//^nice masnory grid layout
+
 
 export default FeateuredCars;
