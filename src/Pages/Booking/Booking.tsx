d1f1f79cb1a9b2a971646fee2d90feb20db5de7a
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-10-03 00:21:19 +0600

    car booking page is done but the details page and the auth part is not

--- src/Pages/Booking/Booking.tsx
+++ src/Pages/Booking/Booking.tsx
@@ -1,10 +1,72 @@
 
-const Booking = () => {
+import Loader from "@/components/shared/Loader";
+import { useEffect, useState } from "react";
+
+import { TCar } from "@/redux/features/Car/carSlice";
+import { useGetAllCarsQuery } from "@/redux/features/Car/carApi";
+import Lottie from "lottie-react";
+import NoData from './../../assets/NoDataFound.json'
+import CarFilter from "../Cars/CarFilter/CarFilter";
+import BookingCard from "./BookingCard";
+
+interface QueryParams {
+  search?: string;
+  pricePerHour?: string;
+  model?: string;
+  sort?: string;
+  location?: string;
+}
+
+const Car = () => {
+  const [queryParams, setQueryParams] = useState<QueryParams>({});
+  // console.log(queryParams)
+  const transformedArray = Object.keys(queryParams).map((key) => ({
+    name: key,
+    value: queryParams[key as keyof QueryParams],
+  }));
+
+  const { data, refetch, isLoading } = useGetAllCarsQuery(transformedArray);
+
+  useEffect(() => {
+    refetch();
+  }, [queryParams, refetch]);
+
+  if (isLoading) return <Loader />;
+
   return (
-       <div>
-           <h1>Welcome to Booking Page</h1>
+    <section className="my-4">
+      <div>
+        <h1 className="text-center text-2xl italic underline font-bold text-[#020C29]">
+          Explore the best{" "}
+          <span className=" rounded-md text-orange-500">cars</span> in town
+        </h1>
+      </div>
+      <div className="mx-auto px-4 mt-10">
+        <div className="flex flex-col lg:flex-row gap-8">
+          {/* Filters Section */}
+          <div className="lg:w-1/4 ">
+            <CarFilter
+              setQueryParams={setQueryParams}
+              queryParams={queryParams}
+            />
+          </div>
+
+          {/* Cars Listing Section */}
+          <div className="lg:w-3/4 grid grid-cols-1  md:grid-cols-2 gap-8">
+            {data?.data?.length === 0 ? (
+             <div className="flex justify-center items-center">
+             <Lottie  animationData={NoData} loop={true} />
+             </div>
+            ) : (
+              data?.data?.map((car: TCar) => (
+                <BookingCard key={car._id} car={car} />
+              ))
+            )}
+          </div>
         </div>
-);
+      </div>
+    </section>
+  );
 };
 
-export default Booking;
+export default Car;
---
+++ src/Pages/Booking/BookingCard.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { TCar } from "@/redux/features/Car/carSlice";
+
+import { Link } from "react-router-dom";
+
+const BookingCard = ({ car }: { car: TCar }) => {
+  return (
+    <div className="lg:max-w-80 h-full rounded-lg border border-gray-200 p-4 flex flex-col gap-4">
+      <div className="image-fluid">
+        <img
+          className="rounded-md magnifier size-80 "
+          src={car?.image}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="space-y-1">
+          <h3 className="text-xl font-semibold text-[#020C29] ">{car?.name}</h3>
+          <p>{car?.description.slice(0, 60) || "Description not available"}</p>
+          <p className="text-[#020C29] dark:text-white">
+            Location: {car?.location}
+          </p>
+          <div className="flex justify-between items-center">
+            <h3 className=" font-semibold text-orange-600">
+              Price Per Hour: <span className="">$ {car?.pricePerHour}</span>
+            </h3>
+          </div>
+        </div>
+
+       <div className="flex flex-col gap-2">
+       <Link to={`/car-details/${car?._id}`}>
+          <Button className="btn-primary w-full">
+            <span className=" relative z-10">See Details</span>
+          </Button>
+        </Link>
+        <Link to={``}>
+          <Button disabled={car?.status==="unavailable"} className={`btn-primary w-full   ${
+              car.status === "unavailable" ? "opacity-50 cursor-not-allowed" : ""
+            }`}>
+            <span className=" relative z-10">Book Now</span>
+          </Button>
+        </Link>
+       </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingCard;
